3cc86a4145f274a737c993df510d00bbd0c5a10f
ghockdyyuru-cmd <ghockdyyuru@gmail.com>
2026-08-09 13:29:35 +0700

    Bump cache version biar update ke-load

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oxxolot-shell-v1';
+const CACHE = 'oxxolot-shell-v2';
 const SHELL = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
